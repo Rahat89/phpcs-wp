c822dda21212d9ba74873511293b8f0d2b99907d
Rahat Hossain <rahat.feni99@gmail.com>
2022-10-23 12:03:41 +0600

    added composer scripts temporarily

---
+++ readme.md
@@ -0,0 +1,9 @@
+# WordPress Coding Standards for PHP_CodeSniffer
+
+Run the following command to check cs issues
+
+``composer check-cs path_to_file``
+
+Run the following command fix issues that can be automatically fix by phpcbf
+
+``composer fix-cs path_to_file``
--- wp-content/plugins/akismet/akismet.php
+++ wp-content/plugins/akismet/akismet.php
@@ -34,7 +34,7 @@ Copyright 2005-2022 Automattic, Inc.
 */
 
 // Make sure we don't expose any info if called directly
-if ( !function_exists( 'add_action' ) ) {
+if ( ! function_exists( 'add_action' ) ) {
 	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
 	exit;
 }
@@ -47,22 +47,22 @@ define( 'AKISMET_DELETE_LIMIT', 10000 );
 register_activation_hook( __FILE__, array( 'Akismet', 'plugin_activation' ) );
 register_deactivation_hook( __FILE__, array( 'Akismet', 'plugin_deactivation' ) );
 
-require_once( AKISMET__PLUGIN_DIR . 'class.akismet.php' );
-require_once( AKISMET__PLUGIN_DIR . 'class.akismet-widget.php' );
-require_once( AKISMET__PLUGIN_DIR . 'class.akismet-rest-api.php' );
+require_once AKISMET__PLUGIN_DIR . 'class.akismet.php';
+require_once AKISMET__PLUGIN_DIR . 'class.akismet-widget.php';
+require_once AKISMET__PLUGIN_DIR . 'class.akismet-rest-api.php';
 
 add_action( 'init', array( 'Akismet', 'init' ) );
 
 add_action( 'rest_api_init', array( 'Akismet_REST_API', 'init' ) );
 
 if ( is_admin() || ( defined( 'WP_CLI' ) && WP_CLI ) ) {
-	require_once( AKISMET__PLUGIN_DIR . 'class.akismet-admin.php' );
+	require_once AKISMET__PLUGIN_DIR . 'class.akismet-admin.php';
 	add_action( 'init', array( 'Akismet_Admin', 'init' ) );
 }
 
-//add wrapper class around deprecated akismet functions that are referenced elsewhere
-require_once( AKISMET__PLUGIN_DIR . 'wrapper.php' );
+// add wrapper class around deprecated akismet functions that are referenced elsewhere
+require_once AKISMET__PLUGIN_DIR . 'wrapper.php';
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
-	require_once( AKISMET__PLUGIN_DIR . 'class.akismet-cli.php' );
+	require_once AKISMET__PLUGIN_DIR . 'class.akismet-cli.php';
 }
